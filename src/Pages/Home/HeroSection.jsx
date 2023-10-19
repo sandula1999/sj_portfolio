917ed6b08d1abf88a553f946096beea582babf68
Sandula Jeewantha <jeewanthaamarakoon@gmail.com>
2023-10-19 15:36:40 +0530

    Completed the Hero Section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,13 +3,13 @@ export default function HeroSection() {
         <div className="hero--section--content--box">
             <div className="hero--section--content">
                 <p className="section--title">Hey, I'm Sandula Jeewantha</p>
-                <h1 className="hero-section-title">
+                <h1 className="hero--section--title">
                     <span className="hero--section--title--color">Full Stack</span>{""}
                     <br />
                     Developer
                 </h1>
                 <p className="hero--section--description">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                     <br /> Similique fugiat accusamus earum inventore quod?
                 </p>
             </div>
